bba73773d1e96c850c2a108e33be92d872a224cb
Sailesh Mishra <saileshmishra164@gmail.com>
2025-09-23 11:25:52 +0530

    feat: add __version__ attribute and bump to 0.1.4

---
+++ src/poetry_adv_fun_test/__init__.py
@@ -0,0 +1,14 @@
+"""
+poetry_adv_fun_test
+A Python library for modeling active and passive electronic components.
+"""
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:  # For Python < 3.8
+    from importlib_metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("poetry-adv-fun-test")
+except PackageNotFoundError:
+    __version__ = "unknown"
